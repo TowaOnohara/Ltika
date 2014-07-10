8ef762a59835e6892d6a245ed0b471cb2e1a8e49
TowaOnohara <jack.jilljp2@gmail.com>
2014-07-10 22:35:24 +0900

    新規追加

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Controls;
+using Microsoft.SPOT.Presentation.Media;
+using Microsoft.SPOT.Presentation.Shapes;
+using Microsoft.SPOT.Touch;
+
+using Gadgeteer.Networking;
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+using Gadgeteer.Modules.GHIElectronics;
+
+namespace Ltika
+{
+    public partial class Program
+    {
+        // This method is run when the mainboard is powered up or reset.   
+        void ProgramStarted()
+        {
+            Debug.Print("Program Started");
+
+            GT.Timer timer = new GT.Timer(500); // every second (500ms)
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        bool value = false;
+        int ledval = 0;
+        void timer_Tick(GT.Timer timer)
+        {
+            Mainboard.SetDebugLED(value = !value);
+            ledval = (ledval++) % 6 + 1;
+            led7R.TurnLightOn(ledval, true);
+        }
+    }
+}
